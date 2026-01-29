916a90aa08305a6b5c08fa760a57124595b645d8
Enohpolyx9078 <hicrawford982@gmail.com>
2026-01-29 09:14:55 -0700

    Started routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './app.css';
+import { BrowserRouter } from 'react-router';
 
 export default function App() {
     return (
@@ -7,9 +8,15 @@ export default function App() {
             <div className="mx-auto max-w-sm">
                 <img className="logo mx-auto" src="../img/go_play_tag.png" alt="Game logo" />
             </div>
-            <main className="flex-centered">
-                App content here
-            </main>
+            <BrowserRouter>
+            </BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/play' element={<Play />} />
+                <Route path='/scores' element={<Scores />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
             <footer>
                 <small>Authored by Howard Crawford</small>
                 <small><a href="https://github.com/Enohpolyx9078/Tag" target="_blank"> Github</a></small>
