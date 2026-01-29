151bd84080b7c4854bc9bedd7a55f35090b02a88
Enohpolyx9078 <hicrawford982@gmail.com>
2026-01-29 09:10:43 -0700

    Started porting ro react compontents

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,18 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className='.body'>App will display here</div>;
+    return (
+        <div className="body">
+            <div className="mx-auto max-w-sm">
+                <img className="logo mx-auto" src="../img/go_play_tag.png" alt="Game logo" />
+            </div>
+            <main className="flex-centered">
+                App content here
+            </main>
+            <footer>
+                <small>Authored by Howard Crawford</small>
+                <small><a href="https://github.com/Enohpolyx9078/Tag" target="_blank"> Github</a></small>
+            </footer>
+        </div>
+    );
 }
