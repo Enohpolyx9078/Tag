a60ee3a24a112e8f04ee08da6618bd42802e58ac
Enohpolyx9078 <hicrawford982@gmail.com>
2026-02-05 10:05:44 -0700

    Reactivity to login/create account

--- src/create-account/create-account.jsx
+++ src/create-account/create-account.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function CreateAccount() {
-  return (
-    <main className="flex-centered">
-        <h2 className="text-2xl font-semibold">Create Account</h2>
-        <div className="card md:w-100">
-            <label for="username">Username</label>
-            <input className="border-2 border-white" id="username" />
-            <label for="password">Password</label>
-            <input className="border-2 border-white" id="password" type="password" />
-            <NavLink className="main-button" to="/profile"> Create Account</NavLink>
-            <div className="centered">
-                <small>
-                    Already have an account?
-                    <NavLink to="/"> Login</NavLink>
-                </small>
-            </div>
-        </div>
-    </main>
-  );
+  const [userName, setUserName] = React.useState(localStorage.getItem("tagStartup-userName") || '');
+      const [password, setPassword] = React.useState('');
+  
+      async function onLogin() {
+          localStorage.setItem("tagStartup-userName", userName);
+      }
+  
+      return (
+          <main className="flex-centered">
+              <h2 className="text-2xl font-semibold">Create Account</h2>
+              <div className="card md:w-100">
+                  <label htmlFor="username">Username</label>
+                  <input className="border-2 border-white" id="username" onChange={(e) => setUserName(e.target.value)}/>
+                  <label htmlFor="password">Password</label>
+                  <input className="border-2 border-white" id="password" type="password" onChange={(e) => setPassword(e.target.value)} />
+                  <NavLink onClick={ () => onLogin() } className="main-button" to="profile" disabled={!userName || !password}>Create Account</NavLink>
+                  <div className="centered">
+                      <small>
+                          Already have an account?
+                          <NavLink to="/"> Login</NavLink>
+                      </small>
+                  </div>
+              </div>
+          </main>
+    );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -13,9 +13,9 @@ export function Login() {
         <main className="flex-centered">
             <h2 className="text-2xl font-semibold">Login</h2>
             <div className="card md:w-100">
-                <label for="username">Username</label>
+                <label htmlFor="username">Username</label>
                 <input className="border-2 border-white" id="username" defaultValue={ userName } onChange={(e) => setUserName(e.target.value)}/>
-                <label for="password">Password</label>
+                <label htmlFor="password">Password</label>
                 <input className="border-2 border-white" id="password" type="password" onChange={(e) => setPassword(e.target.value)} />
                 <NavLink onClick={ () => onLogin() } className="main-button" to="profile" disabled={!userName || !password}>Login</NavLink>
                 <div className="centered">
