caf5d16008a6fe392fff890e2a962a794c578de9
Enohpolyx9078 <hicrawford982@gmail.com>
2026-02-10 08:21:58 -0700

    Enforce arena boundaries

--- src/game/arena.jsx
+++ src/game/arena.jsx
@@ -4,6 +4,8 @@ import './game-screen.css';
 export function Arena({ skin }) {
     const [position, setPosition] = React.useState({ x: 10, y: 10 });
     const size = 50;
+    const fieldSize = 500;
+    const padding = (3 * 2) + 5; // the border (3) of the character and the arena (5)
 
     const requestRef = React.useRef();
     const keysPressed = React.useRef({});
@@ -15,11 +17,30 @@ export function Arena({ skin }) {
             let { x, y } = prev;
             const speed = 5;
             //TODO use delta Time for position change instead of move speed
+            const moveUp = () => {
+                if (y - speed < 0) y = 0;
+                else y -= speed;
+            }
 
-            if (keysPressed.current['ArrowUp']) y -= speed;
-            if (keysPressed.current['ArrowDown']) y += speed;
-            if (keysPressed.current['ArrowLeft']) x -= speed;
-            if (keysPressed.current['ArrowRight']) x += speed;
+            const moveDown = () => {
+                if (y + speed > fieldSize - size - padding) y = fieldSize - size - padding;
+                else y += speed;
+            }
+
+            const moveLeft = () => {
+                if (x - speed < 0) x = 0;
+                else x -= speed;
+            }
+
+            const moveRight = () => {
+                if (x + speed > fieldSize - size - padding) x = fieldSize - size - padding;
+                else x += speed;
+            }
+
+            if (keysPressed.current['ArrowUp']) moveUp();
+            if (keysPressed.current['ArrowDown']) moveDown();
+            if (keysPressed.current['ArrowLeft']) moveLeft();
+            if (keysPressed.current['ArrowRight']) moveRight();
 
             return { x, y };
         });
