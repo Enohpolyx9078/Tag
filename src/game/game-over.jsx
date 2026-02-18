15246d4f540f218dbd1e187640c94fc6c2db882e
Enohpolyx9078 <hicrawford982@gmail.com>
2026-02-18 13:14:47 -0700

    Corrected game over styles

--- src/game/game-over.jsx
+++ src/game/game-over.jsx
@@ -6,12 +6,12 @@ export function GameOver({ players, skins, winner }) {
     return (
         <div className="relative flex items-center justify-center">
             <ConfettiRain />
-            <div className="absolute">
+            <div className="absolute max-w-[500px]">
                 <div className="flex flex-row items-center">
-                    <svg id="selected" className="skin-big">
+                    <svg id="selection" className="skin-big">
                         <rect x="0" y="0" width="100" height="100" stroke={skins[winner].outline} strokeWidth="12" fill={skins[winner].fill} />
                     </svg>
-                    <h2 className="text-2xl md:text-5xl font-semibold">{players[winner].name} won!</h2>
+                    <h2 className="text-2xl md:text-5xl font-semibold size-min">{players[winner].name} won!</h2>
                 </div>
             </div>
         </div>
