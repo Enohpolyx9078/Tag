ec5399160a464225586540b53fa2e51b092d0fc4
Enohpolyx9078 <hicrawford982@gmail.com>
2026-03-04 11:28:39 -0700

    Set up router and mocked out endpoints

--- service/index.js
+++ service/index.js
@@ -5,8 +5,25 @@ const uuid = require('uuid');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+var apiRouter = express.Router();
 
+app.use(express.json());
+app.use(cookieMonster());
 app.use(express.static('public'));
+app.use(`/api`, apiRouter);
+
+/*
+    TODO: Set up the endpoints
+    Login (PUT)
+    Logout (DELETE)
+    Create (POST)
+    Change Skin (PUT)
+    Join Game (GET)
+    Create Game (POST)
+    2 Player Game (POST)
+    Start Game (GET)
+    Leave Game (GET)
+*/
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
