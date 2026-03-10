4807ba1ac77f951dbe0adcfe50ca070680c68cef
Enohpolyx9078 <hicrawford982@gmail.com>
2026-03-10 11:50:50 -0600

    Placeholder refresh button

--- src/create-game/create-game.jsx
+++ src/create-game/create-game.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { joinRoom, leaveRoom } from '../lib/lib-requests';
+import { fetchRoom, joinRoom, leaveRoom } from '../lib/lib-requests';
 
 export function CreateGame() {
     // playerInit will hold these objects {name: String, skin: skin}
@@ -22,6 +22,11 @@ export function CreateGame() {
     }
 
     // placeholder stuff for WebSocket features later
+    async function refresh() {
+        const init = await fetchRoom(roomCode.current);
+        setPlayerInit(init.playerInit);
+    }
+
     // made with help from Gemini 3
     /* React.useEffect(() => {
         const timeouts = [];
@@ -55,6 +60,7 @@ export function CreateGame() {
             <h2 className="centered text-2xl">Room: {roomCode.current}</h2>
             <div className="card md:w-100">
                 <h2 className="centered text-xl">Players: {playerInit.length}/4</h2>
+                <button onClick={() => refresh()} className="outline-button">Refresh</button>
                 {/* Map loop made with help from Gemini 3 */}
                 {playerInit.map((player, index) => (
                     <div key={index} className="flex flex-col flex-row flex-wrap items-center mb-4">
